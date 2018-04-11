c9e93ebd20080dc704532c12fd7dce59eca05d2c
mizuki.amano <ec2-user@ip-172-31-40-231.us-east-2.compute.internal>
2018-04-11 08:25:53 +0000

    コミットメッセージ

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -46,9 +46,15 @@ class TasksController < ApplicationController
     flash[:success] = 'Taskは正常に削除されました'
     redirect_to tasks_url
   end
-end
-
 
 def task_params
     params.require(:task).permit(:content)
 end
+
+
+end
+
+
+
+
+
